383661fc7ac28b99b6614157431c44cf3f20dd0f
OhSeongHyeon <wert3976@gmail.com>
2025-05-25 20:14:44 +0900

    해치웠나?

---
+++ database/sql/growspace_dml.sql
@@ -0,0 +1,11 @@
+-- =================================
+-- growspace project
+-- growspace_dml.sql
+-- dml 쿼리는 이쪽에서 작성한다.
+-- =================================
+
+-- DROP DATABASE IF EXISTS growspace;
+-- CREATE DATABASE growspace;
+-- USE growspace;
+
+
